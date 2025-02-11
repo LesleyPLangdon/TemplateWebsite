ce8e8647ce0724355e7c1e0603b534c921f0bc58
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2025-02-11 13:04:22 +0000

    nav stuff

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    // to slide content to the right
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    // to slide content to the right
+    document.getElementById("main").style.marginLeft = "0";
+  }
